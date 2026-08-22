b0734be49ed0871a7322cbbe1da11adc139e599c
Adriel_Carvalho <ac2perfil@gmail.com>
2026-08-22 17:52:31 -0300

    Projeto CI Manager - Etapa 1: Extração de dados de uma CI

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ ci = """
 ENTREGA DE ASSISTENCIA TECNICA - CLIENTE
 CLIENTE: ADRIEL CARVALHO 
 DATA: 13/08
-ENDEREÇO: AVENIDA EXEMPLO, 520 - CAMPINAS - SÃO PAULO
+ENDEREÇO: AVENIDA EXEMPLO, 520 - GUARUJÁ - SÃO PAULO
 """
 
 def extrair_cliente(ci):
@@ -34,3 +34,31 @@ def extrair_tipo(ci):
         return "ENTREGA"
     elif "RETIRADA" in ci:
         return "RETIRADA"
+
+def extrair_bairro(ci):
+    endereco = extrair_endereco(ci)
+    partes = endereco.split("-")
+    bairro = partes[1].strip()
+    return bairro
+
+def is_viagem(ci):
+    locais_viagem = ["CAMPINAS", "PORTO FELIZ", "CARAGUATATUBA", "RIVIERA", "GUARUJÁ"]
+    bairro = extrair_bairro(ci)
+    for local in locais_viagem:
+        if local in bairro:
+            return True
+    return False
+
+def processar_ci(ci):
+    dados = {
+        "cliente": extrair_cliente(ci),
+        "tipo": extrair_tipo(ci),
+        "data": extrair_data(ci),
+        "endereco": extrair_endereco(ci),
+        "bairro": extrair_bairro(ci),
+        "viagem": is_viagem(ci)
+    }
+    return dados
+
+resultado = processar_ci(ci)
+print(resultado)
